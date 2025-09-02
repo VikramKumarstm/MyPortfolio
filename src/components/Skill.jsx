e2046da1b7d9b46a7d490d42c3a3d42ac518bebe
VikramKumarstm <vikramclg0000@gmail.com>
2025-09-02 16:41:30 +0530

    make responsive skills section

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,7 +5,7 @@ function Skill() {
     <div className='w-full h-full py-16 bg-[#0F0E47] text-white flex flex-col items-center'>
         <h1 className='text-3xl font-semibold w-full text-center'>My Skills</h1>
         <p className='w-[150px] h-[2px] rounded-2xl bg-indigo-500'></p>
-        <div className='w-[80%] pt-8 flex justify-between'>
+        <div className='w-[80%] pt-8 sm:flex justify-between'>
             <div className="basis-1/2">
                 <div className='w-[90%] h-[40px] rounded-[5px] border-2 border-indigo-500 mb-4'>
                     <div className='w-[90%] h-full rounded-[3px] bg-indigo-500 flex items-center'>
